c805251a82219560f393548024485b926575d867
antruc <admin@antruc.dev>
2026-05-15 11:07:16 -0600

    Fix base path

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,5 @@
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  publicDir: 'public',
-  css: {
-    devSourcemap: true
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    base: '/notes/',
-    rollupOptions: {
-      input: {
-        index: resolve(__dirname, 'index.html') // eslint-disable-line
-      }
-    }
-  }
+  base: '/notes/',
 })

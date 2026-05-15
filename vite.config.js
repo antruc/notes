e6d2d7d9a77ff35ffca5fa275f276bfe88d7a8d2
antruc <admin@antruc.dev>
2026-05-15 10:38:00 -0600

    Add base to Vite

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   publicDir: 'public',
   css: {
-    devSourcemap: true,
+    devSourcemap: true
   },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    base: '/notes/',
     rollupOptions: {
       input: {
-        index:     resolve(__dirname, 'index.html'),
-      },
-    },
-  },
-});
+        index: resolve(__dirname, 'index.html') // eslint-disable-line
+      }
+    }
+  }
+})
